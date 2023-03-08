194e2d5a8dd092b58260753af3360c28e58ed5ca
Joseph Perez <joseph@p14.dev>
2023-03-07 16:01:19 -0800

    official setup

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-export function capitalize(string) {
+export function capitalize(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 };
 
-export function shuffle(array) {
-  let currentIndex = array.length;
-  let temporaryValue, randomIndex;
+export function shuffle(array: any[]): any[] {
+  let currentIndex: number = array.length;
+  let temporaryValue: any, randomIndex: number;
 
   while (0 !== currentIndex) {
     randomIndex = Math.floor(Math.random() * currentIndex);
