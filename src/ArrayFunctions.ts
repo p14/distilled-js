7b82889d8fef173ac939b21cd0e3370affafcf37
Joseph Perez <joseph@p14.dev>
2023-03-13 20:01:58 -0700

    Add comments and new string function

--- src/ArrayFunctions.ts
+++ src/ArrayFunctions.ts
@@ -1,3 +1,8 @@
+/**
+ * Randomizes the order of items in an array.
+ * @param array any[]
+ * @returns any[]
+ */
 export function shuffle(array: any[]): any[] {
   let currentIndex: number = array.length;
   let temporaryValue: any, randomIndex: number;
@@ -14,10 +19,22 @@ export function shuffle(array: any[]): any[] {
   return array;
 };
 
+/**
+ * Filters out duplicate items in an array.
+ * @param array any[]
+ * @returns any[]
+ */
 export function unique(array: any[]): any[] {
   return array.filter((v, i, a) => a.indexOf(v) === i);
 };
 
+/**
+ * Filters objects out of an array based on duplication of the provided property value.
+ * If a duplicate is found, only the first instance is returned.
+ * @param array any[]
+ * @param property string
+ * @returns any[]
+ */
 export function uniqueObjectsByProperty(array: any[], property: string): any[] {
   return array.filter((obj, i) => array.findIndex((a) => a[property] === obj[property]) === i);
 };
--- src/StringFunctions.ts
+++ src/StringFunctions.ts
@@ -1,3 +1,21 @@
+/**
+ * Capitalizes the first character of a string and lowercases everything else.
+ * @param string string
+ * @returns string
+ */
 export function capitalize(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+/**
+ * Capitalizes the first character of every word in a string and lowercases everything else.
+ * @param string string
+ * @returns string
+ */
+export function capitalizeEachWord(string: string): string {
+  const eachWord = string.split(' ').map((word) => (
+    word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  ));
+
+  return eachWord.join(' ');
 };
