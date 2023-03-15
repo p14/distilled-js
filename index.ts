228b75d8a7ff8723f984c3c565f84e96bf466865
Joseph Perez <joseph@p14.dev>
2023-03-14 19:56:30 -0700

    Improve exporting process

--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-// export * from './src/ArrayFunctions';
-// export * from './src/MockData';
-// export * from './src/StringFunctions';
-
-export { shuffle, unique, uniqueObjectsByProperty } from './src/ArrayFunctions';
-export { mockStringArray } from './src/MockData';
-export { capitalize, capitalizeEachWord } from './src/StringFunctions';
+export * from './src/ArrayFunctions';
+export * from './src/MockData';
+export * from './src/StringFunctions';
